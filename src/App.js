470c6354f62d3b0808fb1c2b5f378bb0d9217cc4
username <email@example.com>
2025-04-07 07:53:01 +0900

    ルーティングの設定を修正

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from './theme';
 import MBTITest from './components/MBTITest';
@@ -9,15 +9,13 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route path="/mbti-test" element={<MBTITest />} />
-            <Route path="/result" element={<DifyResultScreen />} />
-            <Route path="*" element={<Navigate to="/mbti-test" replace />} />
-          </Routes>
-        </div>
-      </Router>
+      <div className="App">
+        <Routes>
+          <Route path="/mbti-test" element={<MBTITest />} />
+          <Route path="/result" element={<DifyResultScreen />} />
+          <Route path="*" element={<Navigate to="/mbti-test" replace />} />
+        </Routes>
+      </div>
     </ThemeProvider>
   );
 }
